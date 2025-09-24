578ba7f9deee66a5bdab08f7628cf22d812ea655
thewaiter <s.uram@centrum.sk>
2025-09-24 10:42:39 +0200

    Spinner added for data get progress

--- src/e_mod_main.c
+++ src/e_mod_main.c
@@ -491,9 +491,10 @@ _cb_fc_check(void *data)
              lang_buf, inst->ci->host, forecast);
    free(temp);
 
+   edje_object_signal_emit(inst->forecasts_obj, "spin,show", "e");
    url_con = ecore_con_url_new(url);
    if (!url_con) WRN("error when creating ecore con url object.\n");
-
+   
    ecore_con_url_data_set(url_con, inst);
    if (!ecore_con_url_get(url_con))
      {
@@ -504,6 +505,7 @@ _cb_fc_check(void *data)
 
    free_url_con:
    ecore_con_url_free(url_con);
+   edje_object_signal_emit(inst->forecasts_obj, "spin,hide", "e");
    return ECORE_CALLBACK_RENEW;
 }
 
@@ -541,6 +543,7 @@ _cb_url_complete(void *data, int type __UNUSED__, void *event)
      {
        _fc_converter(inst);
        _fc_display_set(inst, ret);
+       edje_object_signal_emit(inst->forecasts_obj, "spin,hide", "e");
      }
    eina_binbuf_reset(inst->buffer);
 
